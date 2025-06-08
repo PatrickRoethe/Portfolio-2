3af838ccafa7979c7f81e8fa06fee2cedc156311
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2025-06-08 17:55:23 +0200

    herobanner on homepage
    imported and added herobanner to home.jsx
    
    created a foundation for herobanner section on home page.

---
+++ src/components/Herobanner.jsx
@@ -0,0 +1,12 @@
+function HeroBanner() {
+  return (
+    <section className="w-full h-[60vh] bg-gradient-to-br from-primary to-secondary text-textLight flex flex-col justify-center items-center text-center px-4">
+      <h1 className="text-4xl md:text-5xl font-bold mb-4">Patrick Røthe</h1>
+      <p className="text-lg md:text-xl font-light">
+        Front-End Developer crafting clean and interactive interfaces
+      </p>
+    </section>
+  );
+}
+
+export default HeroBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import HeroBanner from "../components/Herobanner";
 import ProjectCard from "../components/ProjectCard";
 import { projects } from "../data/project";
 
 function Home() {
   return (
     <div className="p-4">
+      <HeroBanner />
       <h1 className="text-2xl font-bold mb-6">My Projects</h1>
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
